df4745568716d8a00b74c7b55de84473728530d2
Jorrit076 <jorrit-meeuwissen@hotmail.com>
2020-05-22 19:23:06 +0200

    PickupObjects

---
+++ Assets/Scripts/Pickupable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+
+public class Pickupable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
