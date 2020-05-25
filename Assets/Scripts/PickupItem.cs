9d2e47141634dcee49cd2cd4097d3ec4172103aa
Jorrit076 <jorrit-meeuwissen@hotmail.com>
2020-05-25 15:44:30 +0200

    Added sounds

--- Assets/Scripts/PickupItem.cs
+++ Assets/Scripts/PickupItem.cs
@@ -6,11 +6,19 @@ public class PickupItem : MonoBehaviour {
 	private PlayerInputActions _inputActions;
 	private bool _carried = false;
 	private GameObject _nearbyPlayer = null;
+
+	private AudioSource[] sounds;
+	private AudioSource pickupSound;
+	private AudioSource dropSound;
+
 	
 	
 	void Awake() {
 		_inputActions = new PlayerInputActions();
 		_inputActions.PlayerControls.Interact.performed += Pickup;
+		sounds = GetComponents<AudioSource>();
+		pickupSound = sounds[0];
+		dropSound = sounds[1];
 	}
 
 	private void OnTriggerEnter(Collider other) {
@@ -29,12 +37,14 @@ public class PickupItem : MonoBehaviour {
 	    if (_nearbyPlayer!= null && !_carried) {
 		    if (_nearbyPlayer.GetComponent<PlayerController>().Pickup(this)) {
 			    _carried = true;
+				pickupSound.Play();
 		    }
 	    }
     }
 
     public void Drop() {
 	    _carried = false;
+		dropSound.Play();
     }
     
     private void OnEnable() {
