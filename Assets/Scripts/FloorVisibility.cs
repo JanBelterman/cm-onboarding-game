e39b25c9f1058681490ff5b2e703360580d87f8c
Chris <christian-boer@hotmail.com>
2020-06-02 04:36:35 +0200

    Map materials and post processing added

---
+++ Assets/Scripts/FloorVisibility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorVisibility : MonoBehaviour {
+    public GameObject[] invisibleFloors;
+
+    // Player enters floor
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            // Activate all children (make floor visible)
+            foreach (Transform child in transform) {
+                child.gameObject.SetActive(true);
+            }
+            
+            // Deactivate all children of invisible floors
+            if (invisibleFloors.Length > 0) {
+                foreach (GameObject floor in invisibleFloors) {
+                    foreach (Transform child in floor.transform) {
+                        child.gameObject.SetActive(false);
+                    }
+                }
+            }
+        }
+    }
+}
