6baba5b89475bd505fa29262d915f098feb65c9c
Chris <christian-boer@hotmail.com>
2020-06-09 04:33:14 +0200

    Quests setup

---
+++ Assets/Scripts/Quest.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Quest : ScriptableObject {
+    // public int questId;
+    public string title;
+    [TextArea]
+    public string description;
+}
