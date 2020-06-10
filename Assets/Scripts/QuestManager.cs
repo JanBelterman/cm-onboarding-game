dd4db966de9154210f79a5b717ce62a483c62228
Jan Belterman <jjabelte@avans.nl>
2020-06-10 16:08:36 +0200

    Added current quest hud

--- Assets/Scripts/QuestManager.cs
+++ Assets/Scripts/QuestManager.cs
@@ -4,7 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class QuestManager : MonoBehaviour {
+public class QuestManager : MonoBehaviour
+{
     public List<Quest> activeQuests;
     public List<Quest> completedQuests;
 
@@ -13,32 +14,38 @@ public class QuestManager : MonoBehaviour {
     [HideInInspector] public string enteredPuzzle = null;
     private UIManager _uiManager;
     private PickupController _pickupController;
-    
-    private void Awake() {
+
+    private void Awake()
+    {
         activeQuests = new List<Quest>();
         completedQuests = new List<Quest>();
         _uiManager = FindObjectOfType<UIManager>();
         _pickupController = FindObjectOfType<PickupController>();
     }
 
-    public IEnumerator AcceptQuest(Quest quest) {
+    public IEnumerator AcceptQuest(Quest quest)
+    {
         if (completedQuests.Contains(quest)) yield break;
-        if (activeQuests.Contains(quest))  yield break;
+        if (activeQuests.Contains(quest)) yield break;
         int requiredQuestCount = 0;
-        foreach (QuestData requiredQuest in quest.QuestData.requiredQuests) {
-            foreach (Quest completedQuest in completedQuests) {
+        foreach (QuestData requiredQuest in quest.QuestData.requiredQuests)
+        {
+            foreach (Quest completedQuest in completedQuests)
+            {
                 if (completedQuest.QuestData == requiredQuest) requiredQuestCount++;
             }
         }
-        if (requiredQuestCount != quest.QuestData.requiredQuests.Length && quest.QuestData.requiredQuests.Length > 0)  
+        if (requiredQuestCount != quest.QuestData.requiredQuests.Length && quest.QuestData.requiredQuests.Length > 0)
             yield break;
 
         _uiManager.ShowQuest(quest);
-        
-        while (!acceptClicked && !declineClicked) {
+
+        while (!acceptClicked && !declineClicked)
+        {
             yield return null;
         }
-        if (declineClicked) {
+        if (declineClicked)
+        {
             declineClicked = false;
             yield break;
         }
@@ -46,27 +53,35 @@ public class QuestManager : MonoBehaviour {
 
         Debug.Log($"Quest: \"{quest.QuestData.title}\" added to active quests.");
         activeQuests.Add(quest);
+        _uiManager.showCurrentQuest(quest);
     }
 
-    public IEnumerator CompleteQuest(NPC completionNPC, PickupItem heldItem) {
+    public IEnumerator CompleteQuest(NPC completionNPC, PickupItem heldItem)
+    {
         List<Quest> tempCompletedQuests = new List<Quest>();
-        foreach (Quest questMatch in activeQuests.Where(quest => quest.deliveryNPC == completionNPC)) {
-            if (questMatch.requiredItem == heldItem) {
+        foreach (Quest questMatch in activeQuests.Where(quest => quest.deliveryNPC == completionNPC))
+        {
+            if (questMatch.requiredItem == heldItem)
+            {
                 _uiManager.ShowPuzzle();
-                
-                while (!enteredPuzzle.Equals(questMatch.QuestData.puzzleAnswer.Trim())) {
-                    while (String.IsNullOrEmpty(enteredPuzzle)) {
+
+                while (!enteredPuzzle.Equals(questMatch.QuestData.puzzleAnswer.Trim()))
+                {
+                    while (String.IsNullOrEmpty(enteredPuzzle))
+                    {
                         yield return null;
                     }
-                    
-                    if (!enteredPuzzle.Equals(questMatch.QuestData.puzzleAnswer.Trim())) {
+
+                    if (!enteredPuzzle.Equals(questMatch.QuestData.puzzleAnswer.Trim()))
+                    {
                         StartCoroutine(_uiManager.PuzzleIncorrect());
                         enteredPuzzle = String.Empty;
                     }
-                } 
-                
+                }
+
                 enteredPuzzle = String.Empty;
                 _uiManager.PuzzleCorrect();
+                _uiManager.hideCurrentQuest();
 
                 completedQuests.Add(questMatch);
                 tempCompletedQuests.Add(questMatch);
@@ -76,14 +91,16 @@ public class QuestManager : MonoBehaviour {
             }
         }
 
-        foreach (Quest quest in tempCompletedQuests) {
+        foreach (Quest quest in tempCompletedQuests)
+        {
             activeQuests.Remove(quest);
         }
 
         yield return null;
     }
 
-    public void StopCoroutines() {
+    public void StopCoroutines()
+    {
         StopAllCoroutines();
     }
 }
